189430dd2ea25d04e7cc5cd0ba7601354a10032a
mr-dorian-black <mr-dorian-black@users.noreply.github.com>
2023-07-23 18:47:33 +0300

    v1.1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,41 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const images = galleryItems.map(item => {
+     let liItem = document.createElement('li');
+     liItem.classList.add('gallery__item');
+
+     let aItem = document.createElement('a');
+     aItem.classList.add('gallery__link');
+     aItem.href = item.original;
+
+     let imgItem = document.createElement('img');
+     imgItem.classList.add('gallery__image');
+     imgItem.src = item.preview;
+     imgItem.alt = item.description;
+
+     aItem.append(imgItem);
+     liItem.append(aItem);
+     return liItem
+});
+
+gallery.append(...images);
+
+let options = {
+     captionsData: 'alt',
+     captionDelay: 250,
+};
+let lightbox = new SimpleLightbox('.gallery a', options);
+
+function openImage(event) {
+     event.preventDefault();
+     lightbox.open();
+}
+
+
+gallery.addEventListener('click', openImage);
